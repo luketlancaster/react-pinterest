31e344d2f5bccc462e861becbd0365dbae54f239
Luke Lancaster <luketlancaster@gmail.com>
2019-12-11 09:16:56 -0600

    Firebase auth setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase/app';
+// import 'firebase/auth';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+import firebaseConnection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+firebaseConnection.firebaseApp();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    return (
+      < div className = "App" >
+        <h1>App</h1>
+        {
+          (this.state.authed)
+            ? (<div>Already Authed</div>)
+            : (<Auth />)
+        }
+      </div >
+    );
+  }
 }
 
 export default App;
